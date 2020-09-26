53cfcc2af6b717a6dcb2d4f5906bafd40fbf166c
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-09-26 19:31:23 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="global-wrapper">
+    <h1>Under Construction</h1>
+
+    <img src="~assets/welcome-banner.gif">
+
+    <img src="~assets/under-construction-2.gif">
+    <img src="~assets/under-construction-1.gif">
+    <img src="~assets/under-construction-3.gif">
+    <img src="~assets/under-construction-4.gif">
+
+    <img src="~assets/surf-cpu.gif">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$img-space: 10px;
+
+.global-wrapper {
+  display: flex;
+  flex-direction: column;
+
+  > * {
+    align-self: center;
+  }
+}
+
+img {
+  display: block;
+  margin-bottom: $img-space;
+}
+</style>
