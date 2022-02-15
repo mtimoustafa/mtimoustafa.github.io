b001b3e5051566cd04a323ae6f532c08115157a7
Mohamed Moustafa <mti.moustafa@gmail.com>
2022-02-15 16:06:13 +0100

    Test some utility classes!

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
-<main class="container mx-auto">
-  <h1 class="text-3xl font-bold underline">Hi! I'm Mohamed.</h1>
+<main class="container mx-auto h-screen ">
+  <div class="flex justify-center items-center space-x-16 py-36">
+    <img src="https://s.gravatar.com/avatar/de0de92608032415a6880c65732ebc7d?s=300" class="rounded-full" />
 
-  <p>
-    I'm a software developer with a focus on accessibility. I'm a Canadian living in Utrecht, Netherlands. I like to tinker and experiment with anything - tech-related or not - and hope to have my own lush digital garden in this corner of the internet ☺️
-  </p>
+    <div class="flex flex-col max-w-md space-y-4">
+      <h1 class="text-5xl font-bold italic">Hi! I'm Mohamed.</h1>
+
+      <p>
+        I'm a software developer with a focus on accessibility. I'm a Canadian living in Utrecht, Netherlands. I like to tinker and experiment with anything - tech-related or not - and hope to have my own lush digital garden in this corner of the internet ☺️
+      </p>
+    </div>
+  </div>
 </main>
 
 <script>
