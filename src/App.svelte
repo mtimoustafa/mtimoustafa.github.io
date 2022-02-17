664390da6aeed65a85b7daec10d8394ecf7e6939
Mohamed Moustafa <mti.moustafa@gmail.com>
2022-02-18 00:01:40 +0100

    Add favicon + title, little blurb with links to start :)

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,44 @@
-<main class="container mx-auto h-screen ">
-  <div class="flex justify-center items-center space-x-16 py-36">
+<main class="container mx-auto h-screen text-white-500">
+  <div class="flex justify-center items-start space-x-16 py-36">
     <img src="https://s.gravatar.com/avatar/de0de92608032415a6880c65732ebc7d?s=300" class="rounded-full" />
 
-    <div class="flex flex-col max-w-md space-y-4">
-      <h1 class="text-5xl font-bold italic">Hi! I'm Mohamed.</h1>
+    <div class="max-w-md space-y-4">
+      <h1 class="text-5xl font-bold text-sky-500">Hi! I'm Mohamed,</h1>
+
+      <p>an Egyptian-born Canadian living in the Netherlands.</p>
+
+      <p>I work as a full-stack software developer, with a focus on building accessible and delightfully usable applications. At heart, I love tinkering and experimenting with things, and creating something that makes the world a better place.</p>
+
+      <p>I also like to make music, cook wholesome things, and curate a little digital garden of interesting links from all over the internet.</p>
 
       <p>
-        I'm a software developer with a focus on accessibility. I'm a Canadian living in Utrecht, Netherlands. I like to tinker and experiment with anything - tech-related or not - and hope to have my own lush digital garden in this corner of the internet ☺️
+        You can find me in the places below. Come say hi &nbsp;☺️
       </p>
+
+      <ul class="list-disc list-inside">
+        {#each places as place}
+          <li>
+            <a
+              href="{ place.link }"
+              target="_blank"
+              rel="noreferrer noopener nofollow"
+              class="text-sky-500 hover:text-white-500"
+            >
+              { place.name }
+            </a>
+          </li>
+        {/each}
+      </ul>
     </div>
   </div>
 </main>
 
 <script>
+  let places = [
+    { name: 'Polywork', link: 'https://www.polywork.com/fairewinds' },
+    { name: 'Twitter', link: 'https://twitter.com/mtimoustafa' },
+    { name: 'Github', link: 'https://github.com/mtimoustafa' },
+  ]
 </script>
 
 <style>
@@ -22,10 +48,5 @@
   :root {
     font-family: 'Source Sans Pro', 'Open Sans', sans-serif;
     background-color: #001219;
-    color: #F1F9F7;
-  }
-
-  h1 {
-    color: #A2FDDD;
   }
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    colors: {
+      'sky': {
+        500: '#A2FDDD',
+      },
+      'white': {
+        500: '#F1F9F7',
+      },
+    },
   },
   plugins: [],
 }
